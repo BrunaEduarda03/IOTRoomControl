453d84a3fee1b422ebeeff55d26742b07879d0b7
= <william.martins@ee.ufcg.edu.br>
2023-10-11 14:14:27 -0300

    filtro dc

--- src/mod_test.cpp
+++ src/mod_test.cpp
@@ -22,8 +22,6 @@
 #include "SSD1306Wire.h"
 #include "mqqt.h"
 
-
-
 // para compilar o demo, modifique o MACRO no include/macros.h
 #define SDA 21
 #define SCL 22
@@ -41,7 +39,9 @@ void task_sct_read(void *param) {
   analogSetAttenuation(ADC_11db);
   for(;;) {
     // ler corrente
-    double Irms = sct.calcIrms(1600);
+    long start = micros();
+    double Irms = sct.calcIrms(180);
+    double avg_time = (1.0e-3f)*(micros() - start)/180;
     // converte valor lido para str
     char message[10], display_message[30];
     sprintf(message, "%2.2f", Irms);
@@ -58,7 +58,8 @@ void task_sct_read(void *param) {
     //   Serial.println("Published");		       // Irms
     // else
     //   Serial.println("Not Published");
-    
+    Serial.println("Irms " + String(Irms) + " | Power" + String(Irms*VRMS) + " | avg time " \
+      + String(avg_time) + "ms");
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
   return;
@@ -66,38 +67,47 @@ void task_sct_read(void *param) {
 
 #define ADC_WIDTH 12                
 #define ADC_COUNT (1<<ADC_WIDTH)    
-#define SCT_SAMPLING_RATE 200                       
+#define SCT_SAMPLING_RATE 10000                       
 #define SCT_MS_DELAY (int)(1000.0f / SCT_SAMPLING_RATE)  
 #define ADC_CALC(x) (3.3f/ADC_COUNT)*x
 
 void sct_read_rms(void* param) {
 
   analogSetWidth(ADC_WIDTH);
+  
   analogSetAttenuation(ADC_11db);
 
   int g_pin = SCT_PIN;
-  int g_offset = ADC_COUNT >> 1;
+  // mean
+  double g_mean = ADC_COUNT >> 1;
   
   char rms_str[8], power_str[8];
   char rms_display[30], power_display[30];
   
-  long sum_sq = 0;
+  double sum_sq = 0;
+
+  float alpha = 0.1;
 
   for(;;) {
     sum_sq = 0;
     // sampling loop
+    long start = micros();
     for(int i = 0; i < SCT_SAMPLING_RATE; i++) {
       // reads signal + dc level
       int dc = analogRead(g_pin);
+      // updates mean online
+      g_mean = (1-alpha)*g_mean + alpha*dc;
       // remove dc level
-      int ac = dc - g_offset;
+      int ac = dc - g_mean;
       // compute squares
       sum_sq  += ac * ac;
-      Serial.print(String(ADC_CALC(dc)) + ",");
+      // Serial.print(String(ADC_CALC(dc)) + ",");
       vTaskDelay(SCT_MS_DELAY / portTICK_PERIOD_MS);
     }
-    Serial.println(" ");
-    vTaskDelay(100);
+    long total = micros() - start;
+    Serial.println("Offset: " + String(ADC_CALC(g_mean)));
+    // Serial.println(" ");
+    // vTaskDelay(100);
     double rms = sqrt(sum_sq / SCT_SAMPLING_RATE);
     double rms_conv = rms * (3.3 / ADC_COUNT);
     
@@ -105,7 +115,6 @@ void sct_read_rms(void* param) {
 
     sprintf(rms_str, "%2.2lf", rms);
     sprintf(power_str, "%2.2lf", rms);
-    
     
     sprintf(rms_display,   "Irms: %2.2lf A | rms: %2.2lf", rms_conv, rms_conv / 30.f);
     sprintf(power_display, "Power: %2.2lf W", rms_conv*VRMS);
@@ -115,12 +124,13 @@ void sct_read_rms(void* param) {
     display.drawString(0, 0, rms_display);
     display.drawString(0, 18, power_display);
     display.display();
-    Serial.println(String(rms_display) + " " + String(power_display));
+    Serial.println(String(rms_display) + " | " + String(power_display) + " | avg time " \
+      + String(1.0e-3f*total/SCT_SAMPLING_RATE) + "ms");
     bool ret_pub1 = mqqt()->publish(TOPIC("corrente"), rms_str);
     bool ret_pub2 = mqqt()->publish(TOPIC("potencia"), power_str);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
-
 
 void setup() {
   
@@ -139,7 +149,7 @@ void setup() {
   // calibração: relação espiras / resistor de carga
   double sct_cal = 1179 / sct_burden ; 
   // double sct_cal = 1800 / 62 ; 
-  sct.current(SCT_PIN, sct_cal);
+  sct.current(SCT_PIN, 1);
 
   display.init();
   display.setContrast(255);
@@ -151,7 +161,7 @@ void setup() {
   display.display();
 
   // xTaskCreate(task_sct_read, "SCT reading", 2*1024, NULL, 10, NULL);
-  xTaskCreate(sct_read_rms, "SCT reading", 2*1024, NULL, 10, NULL);
+  xTaskCreate(sct_read_rms, "SCT reading", 2*1024, NULL, 40, NULL);
 }
 
 void loop() {
